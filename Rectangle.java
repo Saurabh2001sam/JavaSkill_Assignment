f102780323fcdc697c7186661533a97e44025787
Suman Saurabh <suman12345saurabh@gmail.com>
2023-01-15 14:09:02 +0530

    done

---
+++ Rectangle.java
@@ -0,0 +1,11 @@
+
+public class Rectangle {
+    public static void main(String ... args){
+        double length = 5;
+        double breath = 3;
+        double area = length * breath;
+        double perimeter = 2*(length + breath);
+        System.out.println("Area is "+ area);
+        System.out.println("Perimeter is "+perimeter);
+    }
+}
